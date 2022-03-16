ba7d25503f74e705235304147aee7010571bd7cb
Sergio Marcelo Hosan <smhosan@serv.externos.prismamp.com>
2022-03-16 11:11:27 -0300

    acomodando los melones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoClient } from 'mongodb';
+import { DatabaseModule } from './database/database.module';
+
+//----testear conexión ----
+const uri ="mongodb+srv://admin:Ostruca1203@cluster0.rsnsq.mongodb.net/datosprueba?authSource=admin&replicaSet=atlas-z5nctz-shard-0&w=majority&readPreference=primary";
+const client = new MongoClient(uri);
+async function run(){
+  await client.connect();
+  const database = client.db('datosprueba');
+  const pruebaCollection = database.collection('supermercados');
+  const result = await pruebaCollection.find().limit(2).toArray();
+  console.log(result);
+}
+run();
+//----testear conexión ----
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
